b35793f906a336133dc662c446454db1be80724f
SergeiBychkov <sergeysteers@gmail.com>
2020-02-02 16:37:02 +0300

    правки

--- js/main.js
+++ js/main.js
@@ -4,10 +4,12 @@ var ADS_NUMBER = 8;
 var titles = ['Уютное гнездышко для молодоженов', 'Маленькая квартирка рядом с парком', 'Императорский дворец в центре Токио', 'Милейший чердачок', 'Небольшая лавочка в парке', 'Наркоманский притон'];
 var typeOfBuilding = ['place', 'flat', 'house', 'bungalo'];
 var ads = [];
-var pin = document.querySelector('.map__pin');
 var similarAddPinTemplate = document.querySelector('#pin')
 .content
 .querySelector('.map__pin');
+var numberPhoto = 0;
+
+var pinsList = document.querySelector('.map__pins');
 
 var getRandomIntInclusive = function (min, max) {
   min = Math.ceil(min);
@@ -18,25 +20,25 @@ var getRandomIntInclusive = function (min, max) {
 var creationOfParameters = function () {
   ads.push({
     author: {
-      avatar: 'img/avatars/user0' + getRandomIntInclusive(1, 8) + '.png'
+      avatar: 'img/avatars/user0' + (numberPhoto += 1) + '.png'
     },
-      offer: {
-        title: titles[getRandomIntInclusive(0, titles.length)],
-        adress: '600, 350',
-        price: 42000,
-        type: typeOfBuilding[getRandomIntInclusive(0, typeOfBuilding.length)],
-        rooms: 3,
-        guests: 2,
-        checkin: '12:00',
-        checkout: '13:00',
-        features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
-        description: 'Великолепная лавочка прямо в центре парка. Подходит для всех кто любит спать на свежем воздухе.',
-        photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
-      },
-      location: {
-        x: getRandomIntInclusive(0, 1200),
-        y: getRandomIntInclusive(130, 630)
-      }
+    offer: {
+      title: titles[getRandomIntInclusive(0, titles.length)],
+      adress: '600, 350',
+      price: 42000,
+      type: typeOfBuilding[getRandomIntInclusive(0, typeOfBuilding.length)],
+      rooms: 3,
+      guests: 2,
+      checkin: '12:00',
+      checkout: '13:00',
+      features: ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'],
+      description: 'Великолепная лавочка прямо в центре парка. Подходит для всех кто любит спать на свежем воздухе.',
+      photos: ['http://o0.github.io/assets/images/tokyo/hotel1.jpg', 'http://o0.github.io/assets/images/tokyo/hotel2.jpg', 'http://o0.github.io/assets/images/tokyo/hotel3.jpg']
+    },
+    location: {
+      x: getRandomIntInclusive(0, 1200),
+      y: getRandomIntInclusive(130, 630)
+    }
   });
 };
 
@@ -46,11 +48,19 @@ for (var index = 0; index < ADS_NUMBER; index++) {
 
 document.querySelector('.map').classList.remove('map--faded');
 
-var renderPin = function () {
-var pinCreate = similarAddPinTemplate.cloneNode(true);
+var renderPin = function (parameter) {
+  var pinCreate = similarAddPinTemplate.cloneNode(true);
 
-pinCreate.style = 'left:' + location.x + 'px; top:' + location.y + '}px;';
-pinCreate.querySelector('img').setAttribute('src', author.avatar);
-pinCreate.querySelector('img').setAttribute('alt', offer.title);
-return pinCreate;
+  pinCreate.style = 'left:' + parameter.location.x + 'px; top:' + parameter.location.y + 'px;';
+  pinCreate.querySelector('img').setAttribute('src', parameter.author.avatar);
+  pinCreate.querySelector('img').setAttribute('alt', parameter.offer.title);
+  return pinCreate;
 };
+
+var fragment = document.createDocumentFragment();
+
+for (var i = 0; i < ADS_NUMBER; i++) {
+  fragment.appendChild(renderPin(ads[i]));
+}
+
+pinsList.appendChild(fragment);
